6dc929a76aeda117d44e07763baef59265317d0a
Amadeusz <amadi9205@gmail.com>
2024-04-01 10:45:51 +0200

    Add structure style.css

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const hoursElement = document.querySelector('#hours');
+const minutesElement = document.querySelector('#minutes');
+const secondsElement = document.querySelector('#seconds');
+
+const hoursPut = document.querySelector('#hours-put');
+const minutesPut = document.querySelector('#minutes-put');
+const secondsPut = document.querySelector('#seconds-put');
